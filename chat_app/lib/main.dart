4de32b41c978b15ba3e7bf19348a08c2bc1d1bf8
wjdgnsdl213 <wjdgnsdl213@gmail.com>
2022-08-10 22:04:09 +0900

    채팅 어플 로그인 화면

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:chat_app/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting App',
+      theme: ThemeData(
+          primarySwatch: Colors.blue
+      ),
+      home: LoginSignupScreen(),
+    );
+  }
+}
